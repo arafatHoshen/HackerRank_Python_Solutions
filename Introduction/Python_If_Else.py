b06db5e0b38a3eea9329d5e249be76e2ff4cdcc7
Arafat Hoshen <74883556+arafatHoshen@users.noreply.github.com>
2022-09-13 06:56:55 +0800

    Update Python_If_Else.py

--- Introduction/Python_If_Else.py
+++ Introduction/Python_If_Else.py
@@ -1,10 +1,10 @@
 """
-Title     : Python If-Else
-Domain    : Python
-Subdomain : Introduction
-Author    : Arafat Hoshen
-Created   : 12 September 2022
-Problem   : https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+Title           : Python If-Else
+Domain          : Python
+Subdomain       : Introduction
+Author          : Arafat Hoshen
+Created         : 12 September 2022
+Problem         : https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
 Visit My YouTube: https://www.youtube.com/channel/UCV6vGLwmJneo7leWpgjVBDA
 """
 n = int(input())
